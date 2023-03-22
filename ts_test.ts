61830999d463b4080b28943398907e2abe6535c4
AiinuShil3a <Shil3aiinu@outlook.com>
2023-03-22 12:39:25 +0700

    first commit

---
+++ ts_test.ts
@@ -0,0 +1,17 @@
+const calculator = {
+    sum: function(a: number , b: number) {
+        console.log("Hello, " + a.toFixed() + "!!");
+        console.log("Hello, " + b.toFixed() + "!!");
+        return a + b;
+    },
+    diff: function(a: number , b: number) {
+        console.log("Hello, " + a.toFixed() + "!!");
+        console.log("Hello, " + b.toFixed() + "!!");
+        return a - b;
+    },
+    product: function(a: number , b: number) {
+        console.log("Hello, " + a.toFixed() + "!!");
+        console.log("Hello, " + b.toFixed() + "!!");
+    return a * b
+    }
+}
